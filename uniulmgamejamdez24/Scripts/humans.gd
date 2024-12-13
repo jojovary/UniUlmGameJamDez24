3727209269000b1cf062550b54f9750ff25997cc
JoJo <jonathan.buss.aka.jo@gmail.com>
2024-12-13 22:51:54 +0100

    hoomans do flipppp

--- uniulmgamejamdez24/Scripts/humans.gd
+++ uniulmgamejamdez24/Scripts/humans.gd
@@ -10,13 +10,10 @@ func _ready() -> void:
 	$AnimatedSprite2D.play()
 	var flip = randf_range(0,1)
 	if flip > 0.5:
-		$AnimatedSprite2D.flip_h = true
-
+		scale.x = abs(scale.x) * -1
 
 
 func _physics_process(delta):
-	
-	directionFlip()
 	
 	if currentState == state.WALKING:
 		miseryInCage(delta)
@@ -27,7 +24,6 @@ func _physics_process(delta):
 	
 	move_and_slide()
 
-
 func miseryInCage(delta):
 	if not is_on_floor():
 		velocity.y += gravity * delta
@@ -35,12 +31,3 @@ func miseryInCage(delta):
 		speed = speed * -1
 		scale.x = abs(scale.x) * -1
 	velocity.x = speed
-
-
-func directionFlip():
-	if velocity.x < 0:
-		$AnimatedSprite2D.flip_h = true
-		print("aha")
-	elif velocity.x > 0:
-		$AnimatedSprite2D.flip_h = false
-		
