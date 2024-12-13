b151567628aa4a30399bf03ca536ad2ce58a9d57
PatrickPfeifer <95345384+DerPutuck@users.noreply.github.com>
2024-12-13 22:30:03 +0100

    basic player beam

--- uniulmgamejamdez24/Player/player.gd
+++ uniulmgamejamdez24/Player/player.gd
@@ -5,11 +5,26 @@ const SPEED = 300.0
 
 
 func _physics_process(delta: float) -> void:
-	
+	move_player()
+	activate_beam()
+
+func move_player():
 	var direction := Input.get_axis("ui_left", "ui_right")
 	if direction:
 		velocity.x = direction * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
-
+	
 	move_and_slide()
+
+func activate_beam():
+	if !Input.is_action_pressed("activate_beam"):
+		$Beam.scale.y = 0
+		return
+	
+	$Beam.scale.y = 1
+
+func _on_beam_body_entered(body: Node2D) -> void:
+	if body.is_in_group("humans"):
+		# state change
+		pass
